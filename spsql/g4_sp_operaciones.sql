d71bf8a592a02dd3b8477a34667861dd1b5597f7
Diana Zambrano <damari01zambrano@gmail.com>
2020-12-30 13:16:23 -0500

    Correccion sp operaciones se agrega operacion S

--- spsql/g4_sp_operaciones.sql
+++ spsql/g4_sp_operaciones.sql
@@ -1,37 +1,3 @@
-/****************************************************************************/
-/*     Archivo:     g4_sp_operaciones.sp								    */
-/*     Stored procedure: g4_sp_operaciones                                  */
-/*     Base de datos: cobis											        */
-/*     Producto: Taller Banco                                               */
-/*     Disenado por: Diana Zambrano                                         */
-/*     Fecha de escritura: 28-Dic-2020                                      */
-/****************************************************************************/
-/*                            IMPORTANTE                                    */
-/*    Esta aplicacion es parte de los paquetes bancarios propiedad          */
-/*    de COBISCorp.                                                         */
-/*    Su uso no    autorizado queda  expresamente   prohibido asi como      */
-/*    cualquier    alteracion o  agregado  hecho por    alguno  de sus      */
-/*    usuarios sin el debido consentimiento por   escrito de COBISCorp.     */
-/*    Este programa esta protegido por la ley de   derechos de autor        */
-/*    y por las    convenciones  internacionales   de  propiedad inte-      */
-/*    lectual.    Su uso no  autorizado dara  derecho a    COBISCorp para   */
-/*    obtener ordenes  de secuestro o  retencion y para  perseguir          */
-/*    penalmente a los autores de cualquier   infraccion.                   */
-/****************************************************************************/
-/*                           PROPOSITO                                      */
-/* Realiza las operaciones de Depositar, transferir y retirar               */
-/*                                                          			    */
-/****************************************************************************/
-/*                           MODIFICACIONES                                 */
-/*       FECHA          AUTOR                  RAZON                        */
-/*     28-Dic-2020      Diana Zambrano     Version inicial                  */     
-/*                                                                          */
-/*                                                                          */
-/****************************************************************************/
-
-USE cobis
-go
-
 IF OBJECT_ID ('dbo.g4_sp_operaciones') IS NOT NULL
 	DROP PROCEDURE dbo.g4_sp_operaciones
 GO
@@ -39,30 +5,33 @@ GO
 CREATE PROC g4_sp_operaciones 
 
    
-	@s_srv           varchar(30) = NULL,
-   	@s_ssn           int         = NULL,
-   	@s_ssn_branch    int         = 0,
-   	@s_date          datetime    = NULL,
-   	@s_ofi           smallint    = NULL,
-   	@s_user          varchar(30) = NULL,
-   	@s_lsrv          varchar(30) = NULL,
-   	@s_rol           smallint    = 1,
-   	@s_term          varchar(10) = NULL,
-   	@s_org           char(1)     = NULL,
-   	@s_culture       varchar(10) = 'NEUTRAL',
-   	@t_file          varchar(14) = NULL,
-   	@i_operacion     char(1),
-   	@t_trn           INT 	     =99,
-   	@i_numero_cuenta INT,
-   	@i_cliente       VARCHAR(30),
-   	@i_saldo         FLOAT,
-   	@i_tipo_cuenta   CHAR(1)
- 
+	@s_srv                      varchar(30) = NULL,
+   	@s_ssn                      int         = NULL,
+   	@s_ssn_branch               int         = 0,
+   	@s_date                     datetime    = NULL,
+   	@s_ofi                      smallint    = NULL,
+   	@s_user                     varchar(30) = NULL,
+   	@s_lsrv                     varchar(30) = NULL,
+   	@s_rol                      smallint    = 1,
+   	@s_term                     varchar(10) = NULL,
+   	@s_org                      char(1)     = NULL,
+   	@s_culture                  varchar(10) = 'NEUTRAL',
+   	@t_file                     varchar(14) = NULL,
+   	@i_operacion                char(1),
+   	@t_trn                      INT 	    = 99,
+   	@i_numero_cuenta            INT,          
+   	@i_numero_cuenta_destino	INT         =  NULL,
+   	@i_cliente                  VARCHAR(30) = NULL,
+   	@i_valor                    FLOAT		= 0	,
+   	@i_saldo                    FLOAT		= 0	,
+   	@i_tipo_cuenta              VARCHAR(10)	= NULL,
+   	@i_tipo_cuenta_destino	    VARCHAR(10)	= NULL
    
 AS
 
 	DECLARE 
 	        @w_saldoActual FLOAT,
+	        @w_saldoDestino FLOAT,
 	        @w_error       INT,
 	        @w_ced_cliente VARCHAR(30),
 	        @w_nom_cliente VARCHAR(30),
@@ -77,52 +46,51 @@ AS
 --opcion: Q - consultar datos de cuenta
 
 if @i_operacion = 'Q'
-BEGIN
-   IF EXISTS (SELECT 1 FROM g4_cuenta_ahorros WHERE ca_banco = @i_numero_cuenta)
-      BEGIN
-         IF(@i_tipo_cuenta=0)
-         BEGIN
-       
-            SELECT  @w_tipo_cuenta ='Ahorros', 
-                    @w_num_cuenta  = ca_banco,
-                    @w_ced_cliente = cl_cedula,
-                    @w_nom_cliente = cl_nombre,
-                    @w_ape_cliente = cl_apellido,
-                    @w_saldoActual = ca_saldo
-                    
-            FROM    g4_cuenta_ahorros,cliente_taller 
-            WHERE   cl_id= ca_cliente
-            AND     ca_banco = @i_numero_cuenta
-            
-                 
-            
-         END
-         IF(@i_tipo_cuenta=1)
-         BEGIN
-       
-            SELECT  @w_tipo_cuenta='Corriente', 
-                    @w_num_cuenta  = ca_banco,
-                    @w_ced_cliente = cl_cedula,
-                    @w_nom_cliente = cl_nombre,
-                    @w_ape_cliente = cl_apellido,
-                    @w_saldoActual = ca_saldo
-                    
-            FROM    g4_cuenta_corriente,cliente_taller 
-            WHERE   cl_id= ca_cliente
-            AND     ca_banco = @i_numero_cuenta
-          
-         END
-         
-         SELECT 'Tipo_Cuenta'       =@w_tipo_cuenta,
-                'Numero_Cuenta'     =@w_num_cuenta,  
-                'Cedula_Cliente'    =@w_ced_cliente, 
-                'Nombre_Cliente'    =@w_nom_cliente, 
-                'Apellido_Cliente'  =@w_ape_cliente,
-                'Saldo'             = @w_saldoActual
-     
-      END        
-                 
-END
+	BEGIN
+
+	
+		IF (SELECT COUNT(*) from g4_cuenta_ahorros WHERE ca_banco = @i_numero_cuenta )<>0
+		
+		BEGIN
+		
+			SET	@i_cliente = (SELECT [ca_cliente] FROM g4_cuenta_ahorros WHERE ca_banco = @i_numero_cuenta)
+			
+			SELECT ca_banco,
+					ca_cliente,
+			       CAST(ca_saldo AS VARCHAR),
+			       cl_cedula,
+			       cl_nombre,
+			       cl_apellido,
+			      'tipo_cuenta '='Ahorros'
+			       
+		 	FROM g4_cuenta_ahorros INNER JOIN cliente_taller ON cl_id=@i_cliente
+		 	WHERE ca_banco=@i_numero_cuenta
+		 	
+		END
+
+		ELSE IF (SELECT COUNT(*) from g4_cuenta_corriente WHERE ca_banco = @i_numero_cuenta)<>0
+		
+		BEGIN
+		 	
+		 	SET	@i_cliente = (SELECT [ca_cliente] FROM g4_cuenta_corriente WHERE ca_banco = @i_numero_cuenta)
+		 	
+			SELECT ca_banco,
+			      ca_cliente,
+			       CAST(ca_saldo AS VARCHAR),
+			       cl_cedula,
+			       cl_nombre,
+			       cl_apellido,
+			      'tipo_cuenta '='Corriente'
+			       
+		 	FROM g4_cuenta_corriente INNER JOIN cliente_taller ON cl_id=@i_cliente	 	
+		 	WHERE ca_banco=@i_numero_cuenta	
+			
+			
+		END
+		
+		
+	
+	END
 
 
 
@@ -136,7 +104,7 @@ END
 
 if @i_operacion = 'C' 
 BEGIN
-   IF @i_tipo_cuenta = 1
+   IF @i_tipo_cuenta = 'Ahorros'
    BEGIN 
    
       IF EXISTS (SELECT 1 FROM g4_cuenta_ahorros WHERE ca_banco = @i_numero_cuenta AND ca_cliente= @i_cliente)
@@ -151,6 +119,15 @@ BEGIN
             ca_fecha_modificacion = getdate()
         WHERE ca_banco = @i_numero_cuenta 
         AND ca_cliente= @i_cliente
+        
+        INSERT INTO g4_transaccion
+               	(
+               	tr_fecha,            	tr_cuenta,            	tr_tipo_tr,            	tr_tipo_cuenta
+               	)
+                   VALUES 
+               	(
+               	GETDATE(),            	@i_numero_cuenta,            	'D',            	@i_tipo_cuenta
+               	)
     
       END 
       ELSE
@@ -172,7 +149,7 @@ BEGIN
    
    END
    
-   IF @i_tipo_cuenta = 2
+   IF @i_tipo_cuenta = 'Corriente'
    BEGIN
       IF EXISTS (SELECT 1 FROM g4_cuenta_corriente WHERE ca_banco = @i_numero_cuenta AND ca_cliente= @i_cliente)
        BEGIN
@@ -186,10 +163,18 @@ BEGIN
     		     ca_fecha_modificacion = getdate()
     		 WHERE ca_banco = @i_numero_cuenta 
     		 AND ca_cliente= @i_cliente
+    		 
+    		 INSERT INTO g4_transaccion
+               	(
+               	tr_fecha,            	tr_cuenta,            	tr_tipo_tr,            	tr_tipo_cuenta
+               	)
+                   VALUES 
+               	(
+               	GETDATE(),            	@i_numero_cuenta,            	'D',            	@i_tipo_cuenta
+               	)
        
        END
-   END
-   ELSE
+       ELSE
        BEGIN
             SELECT @w_error=4000
 		
@@ -200,11 +185,13 @@ BEGIN
          	@t_from  = 'g4_sp_operaciones',
          	@i_num   = @w_error
          
-         	return 1
+      	return 1
        
        
        
        END
+   END
+   
 	
 
 end
@@ -219,23 +206,43 @@ end
 
 if @i_operacion = 'R' 
 BEGIN
-   IF @i_tipo_cuenta = 1
+ IF @i_tipo_cuenta = 'Ahorros'
    BEGIN 
-   
+      PRINT 'Ingreso a la opcion ahorros'
       IF EXISTS (SELECT 1 FROM g4_cuenta_ahorros WHERE ca_banco = @i_numero_cuenta AND ca_cliente= @i_cliente)
       BEGIN
+        
         SELECT @w_saldoActual = ca_saldo
         FROM g4_cuenta_ahorros 
         WHERE ca_banco = @i_numero_cuenta 
         AND ca_cliente= @i_cliente
+        
+        PRINT @w_saldoActual
+        PRINT @i_saldo
        
        IF @w_saldoActual > @i_saldo
      	 BEGIN
-     	 UPDATE g4_cuenta_ahorros
-     	 SET ca_saldo = @w_saldoActual- @i_saldo,
-     	     ca_fecha_modificacion = getdate()
-     	 WHERE ca_banco = @i_numero_cuenta 
-     	 AND ca_cliente= @i_cliente
+     	    
+     	    PRINT 'Si hay dinero'
+     	    
+     	    UPDATE g4_cuenta_ahorros
+     	    SET ca_saldo = @w_saldoActual- @i_saldo,
+     	        ca_fecha_modificacion = getdate()
+     	    WHERE ca_banco = @i_numero_cuenta 
+     	    AND ca_cliente= @i_cliente
+     	    
+     	    
+     	    INSERT INTO g4_transaccion
+               	(
+               	tr_fecha,            	tr_cuenta,            	tr_tipo_tr,            	tr_tipo_cuenta
+               	)
+                   VALUES 
+               	(
+               	GETDATE(),            	@i_numero_cuenta,            	'R',            	'Ahorros'
+               	)
+               	
+               	
+               	
      	 END
      	 ELSE
      	 BEGIN
@@ -252,28 +259,31 @@ BEGIN
        
      	
     	 END
-      END 
-      ELSE
-       BEGIN
-            SELECT @w_error=4000
-		
-			exec cobis..sp_cerror
-         
-         	@t_debug = 'N',
-         	@t_file  = null,
-         	@t_from  = 'g4_sp_operaciones',
-         	@i_num   = @w_error
-         
-         	return 1
+   END 
+   ELSE
+   BEGIN
+   
+      SELECT @w_error=4000
+	  
+	  exec cobis..sp_cerror
+      
+      @t_debug = 'N',
+      @t_file  = null,
+      @t_from  = 'g4_sp_operaciones',
+      @i_num   = @w_error
+      
+      return 1
        
        
        
-       END
+    END
    
    END
    
-   IF @i_tipo_cuenta = 2
+   IF @i_tipo_cuenta = 'Corriente'
    BEGIN
+      PRINT 'Ingreso a la opcion Corriente'
+      
       IF EXISTS (SELECT 1 FROM g4_cuenta_corriente WHERE ca_banco = @i_numero_cuenta AND ca_cliente= @i_cliente)
        BEGIN
              SELECT @w_saldoActual = ca_saldo
@@ -283,11 +293,25 @@ BEGIN
     		
     		 IF @w_saldoActual > @i_saldo
     		 BEGIN
-    		 UPDATE g4_cuenta_corriente
-    		 SET ca_saldo = @w_saldoActual- @i_saldo,
-    		     ca_fecha_modificacion = getdate()
-    		 WHERE ca_banco = @i_numero_cuenta 
-    		 AND ca_cliente= @i_cliente
+    		    
+    		    
+    		    UPDATE g4_cuenta_corriente
+    		    SET ca_saldo = @w_saldoActual- @i_saldo,
+    		        ca_fecha_modificacion = getdate()
+    		    WHERE ca_banco = @i_numero_cuenta 
+    		    AND ca_cliente= @i_cliente
+    		    
+                
+                
+                INSERT INTO dbo.g4_transaccion
+            	(
+            	tr_fecha,            	tr_cuenta,            	tr_tipo_tr,            	tr_tipo_cuenta
+            	)
+                VALUES 
+            	(
+            	GETDATE(),            	@i_numero_cuenta,            	'R',            	'Corriente'
+            	)
+            
     		 END
     		 ELSE
     		 BEGIN
@@ -296,18 +320,15 @@ BEGIN
 		  		exec cobis..sp_cerror
           		
           		@t_debug = 'N',
-          		@t_file  = null,
+ 		@t_file  = null,
           		@t_from  = 'g4_sp_operaciones',
           		@i_num   = @w_error
           		
           		return 1
         
     		 
-    		 
-    		 END
-       
-       END
    END
+      END 
    ELSE
        BEGIN
             SELECT @w_error=4000
@@ -325,29 +346,147 @@ BEGIN
        
        END
 	
+   END
+	
 
 end
 
 
 
 
-
-
-
-
-
-
-	
-
-
-
-
-
-
 --opcion: T - transferir
-
-
-
+IF	@i_operacion = 'T'
+	
+	BEGIN
+	
+		IF @i_tipo_cuenta = 'Ahorros'
+		
+		BEGIN
+		
+			SET @w_saldoActual = (SELECT [ca_saldo] FROM g4_cuenta_ahorros WHERE ca_banco=@i_numero_cuenta)
+			
+			IF @i_tipo_cuenta_destino = 'Ahorros'
+			
+			BEGIN
+			
+				SET @w_saldoDestino = (SELECT [ca_saldo] FROM g4_cuenta_ahorros WHERE ca_banco=@i_numero_cuenta_destino)
+				
+				UPDATE g4_cuenta_ahorros SET
+				ca_fecha_modificacion = getdate(),
+				ca_saldo			  = (@w_saldoDestino+@i_valor)
+				WHERE ca_banco=@i_numero_cuenta_destino
+			
+			END
+			
+			ELSE IF @i_tipo_cuenta_destino = 'Corriente'
+			
+			BEGIN
+			
+				SET @w_saldoDestino = (SELECT [ca_saldo] FROM g4_cuenta_corriente WHERE ca_banco=@i_numero_cuenta_destino)
+				
+				UPDATE g4_cuenta_corriente SET
+				ca_fecha_modificacion = getdate(),
+				ca_saldo			  = (@w_saldoDestino+@i_valor)
+				WHERE ca_banco=@i_numero_cuenta_destino
+				
+			
+			END
+			
+			UPDATE g4_cuenta_ahorros SET
+				ca_fecha_modificacion = getdate(),
+				ca_saldo			  = (@w_saldoActual-@i_valor)
+				WHERE ca_banco=@i_numero_cuenta
+		
+		END
+		
+		
+		
+		ELSE IF	@i_tipo_cuenta = 'Corriente'
+		
+		BEGIN
+		
+			SET @w_saldoActual = (SELECT [ca_saldo] FROM g4_cuenta_corriente WHERE ca_banco=@i_numero_cuenta)
+			
+			IF @i_tipo_cuenta_destino = 'Ahorros'
+			
+			BEGIN
+			
+				SET @w_saldoDestino = (SELECT [ca_saldo] FROM g4_cuenta_ahorros WHERE ca_banco=@i_numero_cuenta_destino)
+				
+				UPDATE g4_cuenta_ahorros SET
+				ca_fecha_modificacion = getdate(),
+				ca_saldo			  = (@w_saldoDestino+@i_valor)
+				WHERE ca_banco=@i_numero_cuenta_destino
+			
+			END
+			
+			ELSE IF @i_tipo_cuenta_destino = 'Corriente'
+			
+			BEGIN
+			
+				SET @w_saldoDestino = (SELECT [ca_saldo] FROM g4_cuenta_corriente WHERE ca_banco=@i_numero_cuenta_destino)
+				
+				UPDATE g4_cuenta_corriente SET
+				ca_fecha_modificacion = getdate(),
+				ca_saldo			  = (@w_saldoDestino+@i_valor)
+				WHERE ca_banco=@i_numero_cuenta_destino
+				
+			
+			END
+			
+			UPDATE g4_cuenta_corriente SET
+				ca_fecha_modificacion = getdate(),
+				ca_saldo			  = (@w_saldoActual-@i_valor)
+				WHERE ca_banco=@i_numero_cuenta
+		
+		END		
+	
+end
 RETURN 0
 
-go
+
+--opcion: S - Permite consultar transsacciones
+
+   if @i_operacion = 'S'
+	BEGIN
+	   IF EXISTS(SELECT 1 FROM g4_transaccion WHERE tr_cuenta = @i_numero_cuenta)
+	   BEGIN
+	      SELECT 'id'       = tr_id,
+	             'fecha'    = tr_fecha,
+	             'cuenta'   = tr_cuenta,
+	             'tipo_tr'  = tr_tipo_tr,
+	             'tipo_c'   = tr_tipo_cuenta      
+	      FROM g4_transaccion
+	      WHERE tr_cuenta = @i_numero_cuenta
+	
+	   END
+	   ELSE
+	   BEGIN
+	       SELECT @w_error=4000
+		
+		   exec cobis..sp_cerror
+           
+           @t_debug = 'N',
+           @t_file  = null,
+           @t_from  = 'g4_sp_operaciones',
+           @i_num   = @w_error
+           
+           return 1
+	       
+	   
+	   END
+	   
+	   
+	END
+
+ 
+
+
+
+
+
+
+
+
+GO
+
